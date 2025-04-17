a5dd90291995e680f467f79b61582d054dc61352
Sckende <clrccljuhasz@gmail.com>
2025-04-17 15:38:38 -0400

    finission app

--- app.r
+++ app.r
@@ -21,20 +21,11 @@ options(shiny.launch.browser = TRUE)
 #### Local data ####
 # ---------------- #
 # utilisation des terres
-comp <- readRDS("/home/local/USHERBROOKE/juhc3201/BdQc/ReseauSuivi/Data/g15_indicators/results/ESA/2010-2020_comp2010_ISQ_reg.rds")
-
-# donnees prop habitats naturels ISQ
-isq <- read.csv("/home/local/USHERBROOKE/juhc3201/BdQc/ReseauSuivi/Data/g15_indicators/ISQ/QC_meridional_natural_land_prop_2010.csv", h = T, encoding = "latin1")
-isq_nat <- isq[isq$year == 2010, ]
-isq_nat$region[isq_nat$region == "Gaspésie-Îles-de-la-Madeleine"] <- "Gaspésie–Îles-de-la-Madeleine"
-# donnees prop habitats naturels ESA
-esa_nat <- readRDS("/home/local/USHERBROOKE/juhc3201/BdQc/ReseauSuivi/Data/g15_indicators/results/ESA/ESA_prop_hab_nat_2010.rds")
-
+comp <- readRDS("/home/local/USHERBROOKE/juhc3201/BdQc/ReseauSuivi/Data/g15_indicators/results/ESA/2010-2022_land_cover_trend_comp2010_area.rds")
 
 # Several Polygons for Qc
 # -----------------------
 # ---- data pour decoupage admin
-# dec <- st_read("/home/local/USHERBROOKE/juhc3201/BdQc/ReseauSuivi/Data/QUEBEC_regions/BDGA_1M(adm)_SHP/regio_s.shp")
 dec <- st_read("/home/local/USHERBROOKE/juhc3201/BdQc/ReseauSuivi/Data/QUEBEC_regions/QC_admin_reg_simplify.gpkg")
 # selection des regions d'interet
 admin <- dec[dec$RES_NM_REG %in% c(
@@ -64,174 +55,86 @@ server <- function(input, output, session) {
         plot(st_geometry(dec), col = ifelse(dec$RES_NM_REG %in% admin$RES_NM_REG, "grey", "#bebebe67"))
         plot(st_geometry(dec[dec$RES_NM_REG == input$admin_select, ]), col = "red", add = TRUE)
     })
+    observeEvent(input$type_visu, {
+        if (input$type_visu == "Proportion") {
+            updateRadioGroupButtons(session, "range_y_scale", choices = c("Variable", "Fixe"))
+        } else {
+            updateRadioGroupButtons(session, "range_y_scale", choices = "Variable")
+        }
+    })
 
-    # trend plot
-    output$trend_plot <- renderPlot({
+    data <- reactive({
         x <- comp[comp$region == input$admin_select, ]
+        if (input$type_visu == "Proportion") {
+            x$prop_comp_2010
+        } else {
+            x$area_km2
+        }
+    })
 
-        ggplot(
+    output$plot <- renderPlot({
+        x <- comp[comp$region == input$admin_select, ]
+        g <- ggplot(
             data = x,
-            aes(x = year, y = comp_rate, group = IPCC_class, colour = IPCC_class)
+            aes(x = year, y = data(), group = copti_class, colour = copti_class)
         ) +
             geom_line(linewidth = 1) +
-            labs(title = unique(x$region), color = "Catégories") +
+            labs(color = "Catégories") +
             xlab("Année") +
-            ylab("Proportion (%)") +
             scale_x_continuous(n.breaks = length(unique(x$year))) +
             scale_color_manual(
-                labels = c("agriculture", "forêt", "prairie", "autres", "habitation", "milieu humide"),
+                labels = c("agriculture", "autres milieux naturels", "bâti", "eau", "forêt", "milieux humides", "végétation basse"),
                 values = c(
                     "#993300",
-                    "#006600",
-                    "#CC9900",
-                    "#96ac9d",
+                    "#727e07",
                     "#CC0000",
-                    "#019191"
+                    "#012d8b",
+                    "#006600",
+                    "#96ac9d",
+                    "#CC9900"
                 )
             )
-    })
+        if (input$type_visu == "Proportion") {
+            g <- g +
+                ylab("Proportion (%)") +
+                labs(title = "Evolution de la proportion pour chaque type de couverture des terres (comparaison ave 2010)")
 
-    # pie chart
-    output$pie <- renderPlotly({
-        data <- comp[comp$region == input$admin_select & comp$year == 2010, ]
-        colors <- c(
-            "#993300", # agriculture
-            "#006600", # forest
-            "#CC9900", # grassland
-            "#96ac9d", # other
-            "#CC0000", # settlement
-            "#019191" # wetland
-        )
-
-        fig <- plot_ly(data,
-            labels = c("agriculture", "forêt", "prairie", "autres", "habitation", "milieu humide"),
-            # labels = ~IPCC_class,
-            values = ~count_tot,
-            type = "pie",
-            textposition = "inside",
-            textinfo = "label+percent",
-            insidetextfont = list(color = "#FFFFFF"),
-            hoverinfo = "text",
-            text = ~ paste("n = ", count_tot),
-            marker = list(
-                colors = colors,
-                line = list(color = "#FFFFFF", width = 1)
-            ),
-
-            # The 'pull' attribute can also be used to create space between the sectors
-
-            showlegend = FALSE
-        )
-
-        fig <- fig %>% layout(
-            xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
-            yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE)
-        )
-    })
-
-    # comparaison prop habitats naturels ESA vs. ISQ
-    output$hab_nat <- renderPlotly({
-        reg <- input$admin_select
-
-        # data formatting
-        esa <- esa_nat[esa_nat$region == reg, ]
-        esa$data <- "esa"
-        names(esa)[1] <- "nat_hab"
-
-        isq <- isq_nat[isq_nat$region == reg, ]
-        isq$prop <- isq$prop * 100
-        isq$data <- "isq"
-        isq$region <- reg
-
-        cp <- rbind(isq, esa[, c("region", "nat_hab", "prop", "year", "data")])
-        cp <- cp |> mutate(text = sprintf("Données: %s<br> Proportion: %s <br>", data, round(prop, digits = 2)))
-        cp <- cp[order(cp$data, cp$nat_hab), ]
-        cp$nat_hab[cp$nat_hab == "forest"] <- "forêt"
-        cp$nat_hab[cp$nat_hab == "water"] <- "eau"
-        cp$nat_hab[cp$nat_hab == "wetland"] <- "milieu humide"
-
-        # figure
-        fig <- cp %>% plot_ly()
-
-        fig <- fig %>% add_trace(
-            x = cp$nat_hab[cp$data == "esa"],
-            y = cp$prop[cp$data == "esa"],
-            type = "bar",
-            text = cp$text[cp$data == "esa"],
-            textposition = "auto",
-            marker = list(
-                color = c("rgba(46,72,62,0.8)", "rgba(123,181,177, 0.8)", "rgba(166,97,45, 0.8)"),
-                line = list(
-                    color = c("rgba(46,72,62,1)", "rgba(123,181,177, 1)", "rgba(166,97,45, 1)"),
-                    width = 1.5
-                )
-            )
-        )
-
-        fig <- fig %>% add_trace(
-            x = cp$nat_hab[cp$data == "isq"],
-            y = cp$prop[cp$data == "isq"],
-            type = "bar",
-            text = cp$text[cp$data == "isq"],
-            textposition = "auto",
-            marker = list(
-                color = c("rgba(46,72,62,0.5)", "rgba(123,181,177, 0.5)", "rgba(166,97,45, 0.5)"),
-                line = list(
-                    color = c("rgba(46,72,62,1)", "rgba(123,181,177, 1)", "rgba(166,97,45, 1)"),
-                    width = 1.5
-                )
-            )
-        )
-
-        fig <- fig %>%
-            layout(
-                # title = "Comparaison proportion milieux naturels ESA - ISQ",
-                barmode = "group",
-                xaxis = list(title = "Type"),
-                yaxis = list(title = "Proportion (%)"),
-                showlegend = FALSE
-            ) |>
-            style(hoverinfo = "none")
+            if (input$range_y_scale == "Fixe") {
+                g <- g +
+                    ylim(breaks = c(-40, 125))
+            }
+            g
+        } else {
+            g <- g +
+                ylab("Superficie (km2)") +
+                scale_y_log10() +
+                labs(title = "Évolution de la superficie pour chaque type de couverture des terres")
+            g
+        }
     })
 }
-
-ui <- navbarPage(
-    "Évolution de l'utilisation des terres au Qc",
-    sidebarLayout(
-        sidebarPanel(
-            h4("Région administrative"),
-            width = 2,
+ui <- fluidPage(
+    fluidRow(
+        column(
+            3,
             selectInput(
                 inputId = "admin_select",
                 label = "",
                 choices = admin$RES_NM_REG[order(admin$RES_NM_REG)]
-            )
-            # )
-            ,
-            plotOutput("map_ref", width = "100%")
-        ),
-        mainPanel(
-            # First row
-            fluidRow(
-                box(
-                    title = "Évolution utilisation des terres ESA",
-                    width = 8,
-                    plotOutput("trend_plot")
-                ),
-                box(
-                    title = "Proportion de type de terre - ESA 2010",
-                    width = 8,
-                    plotlyOutput("pie")
-                )
             ),
-            fluidRow(
-                box(
-                    title = "Proportion des milieux naturels ESA - ISQ (2010)",
-                    width = 6,
-                    plotlyOutput("hab_nat")
-                )
+            plotOutput("map_ref", width = "100%"),
+            radioGroupButtons("type_visu",
+                label = "Échelle des y",
+                choices = c("Proportion", "Superficie"),
+                selected = "Superficie"
+            ),
+            radioGroupButtons("range_y_scale",
+                label = "Valeurs pour l'axe y",
+                choices = c("Variable", "Fixe"),
+                selected = "Variable"
             )
-        )
+        ),
+        column(9, plotOutput("plot"))
     )
 )
 
--- data_formatting.r
+++ data_formatting.r
@@ -31,20 +31,31 @@ for (i in 1:length(id_names)) {
         items_fetch() |>
         _$features[[which(ids == ids[i])]]$assets[[1]]$href
     print("url retrieved")
+    rast <- rast(paste0("/vsicurl/", url))
     for (j in 1:nrow(reg_conv)) {
-        rast <- rast(paste0("/vsicurl/", url))
+        print(j)
         pol <- reg_conv[j, ]
 
         lc_rast <- crop(rast, pol)
         lc_rast <- mask(lc_rast, pol)
+
         print("crop & mask done")
 
         freq <- as.data.frame(freq(lc_rast))
         freq$layer <- id_names[i]
         freq$year <- as.numeric(substr(id_names[i], 11, 14))
         freq$region <- pol$RES_NM_REG
+        freq$total_pix <- sum(freq$count, na.rm = TRUE)
 
-        cat_tot <- rbind(cat_tot, freq)
+        lc_rast_conv <- project(lc_rast, "EPSG:6624", method = "near") # pour le calcul de l'aire
+        freq2 <- freq(lc_rast_conv)
+        names(freq2)[3] <- "n_pix_proj6624"
+
+        freq3 <- left_join(freq, freq2[, c("value", "n_pix_proj6624")], by = join_by(value))
+        freq3$res6624 <- res(lc_rast_conv)[1]
+        freq3$total_pix6624 <- sum(freq3$n_pix_proj6624, na.rm = TRUE)
+
+        cat_tot <- rbind(cat_tot, freq3)
     }
 }
 
@@ -71,7 +82,11 @@ ll2 <- lapply(ll, function(x) {
         value = missing_cat,
         count = 0,
         year = unique(x$year),
-        region = unique(x$region)
+        region = unique(x$region),
+        total_pix = unique(x$total_pix),
+        n_pix_proj6624 = 0,
+        res6624 = unique(x$res6624),
+        total_pix6624 = unique(x$total_pix6624)
     )
 
     x <- rbind(x, missing_df)
@@ -82,3 +97,46 @@ final <- do.call("rbind", ll2)
 # saveRDS(final, "/home/local/USHERBROOKE/juhc3201/BdQc/ReseauSuivi/Data/g15_indicators/results/ESA/2010-2022_frq_cat0_Qc_region_admin.rds")
 
 # ---- regroupement des categories
+freq <- readRDS("/home/local/USHERBROOKE/juhc3201/BdQc/ReseauSuivi/Data/g15_indicators/results/ESA/2010-2022_frq_cat0_Qc_region_admin.rds")
+# retrait de la region cote nord qui est separee en 4 poly
+freq <- freq[freq$region != "Côte-Nord", ]
+cat <- read.csv("/home/local/USHERBROOKE/juhc3201/BdQc/ReseauSuivi/Indicators/G15_utilisation_terres/CCI-LC_Maps_Legend.csv", h = T)
+
+freq2 <- left_join(freq, cat[, c("cat1", "copti_class")], by = join_by(value == cat1))
+
+f_ll <- split(freq2, list(freq2$region, freq2$year))
+
+f_ll2 <- lapply(f_ll, function(x) {
+    region <- unique(x$region)
+    year <- unique(x$year)
+    total_pix <- unique(x$total_pix)
+    res6624 <- unique(x$res6624)
+
+    summ <- x |>
+        group_by(copti_class) |>
+        summarise(ntot_pix = sum(count, na.rm = TRUE), ntot_pix6624 = sum(n_pix_proj6624, na.rm = TRUE))
+
+    # summ$prop_percent <- (summ$ntot_pix/total_pix) * 100
+    # summ$area_km2 <- (summ$ntot_pix6624*res6624*res6624)/1000000
+    summ$region <- region
+    summ$year <- year
+    summ$total_pix_reg <- total_pix
+    summ$res6624 <- res6624
+    summ
+})
+
+recap <- do.call("rbind", f_ll2)
+
+recap_ll <- split(recap, list(recap$region, recap$copti_class))
+recap_ll2 <- lapply(recap_ll, function(x) {
+    # x$prop_comp_2010 <- x$prop_percent - x$prop_percent[x$year == 2010]
+    # x
+    x$prop_comp_2010 <- (x$ntot_pix - x$ntot_pix[x$year == 2010]) / x$ntot_pix[x$year == 2010] * 100 # suit la methode de calcul de ISQ
+    # x$prop_comp_2010 <- (x$ntot_pix - x$ntot_pix[x$year == 2010]) / unique(x$total_pix_reg) * 100
+    x$area_km2 <- (x$ntot_pix6624 * unique(x$res6624) * unique(x$res6624)) / 1000000
+    x
+})
+
+fin <- do.call("rbind", recap_ll2)
+
+saveRDS(fin, "/home/local/USHERBROOKE/juhc3201/BdQc/ReseauSuivi/Data/g15_indicators/results/ESA/2010-2022_land_cover_trend_comp2010_area.rds")
